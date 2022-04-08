7acbdc5134e3bb437435cca4783e2d2dd8cf4bbd
dfg1425@naver.com <dfg1425@naver.com>
2022-04-08 13:57:09 +0900

    getter setter

---
+++ 5_Getters&Setters.ts
@@ -0,0 +1,20 @@
+class Person5 {
+  public constructor(private _name: string, private age: number) {}   
+  
+  // Get을 하는 함수 getter
+  get name(){ 
+    return this._name = "Sa";  // get은 return 필요
+  }
+
+  // Set을 하는 함수 setter
+  set name(n: string){  // set은 retur이 필수적이진 않고, 받을 인자가 필요하다. 
+    this._name = n;     // 그리고 받은 n을 _name에 넣는다. 
+  }
+}
+
+const p5: Person5 = new Person5("mark", 39);   
+console.log(p5.name);    // get -> 값을 꺼내오는 것
+p5.name = "Seungwon"     // set -> 원래 있던 것들 다른 값으로 설정해주는 것
+console.log(p5.name);    // get, set으로 SeungwonSa 으로 업데이트
+
+
