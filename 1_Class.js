6ae6ecf2271357d41b69c6753c8e835103e8076e
dfg1425@naver.com <dfg1425@naver.com>
2022-04-09 16:38:49 +0900

    compile

--- 1_Class.js
+++ 1_Class.js
@@ -1,5 +1,8 @@
 "use strict";
 class Person {
+    constructor(name) {
+        this.name = name; // name을 프로퍼티로 쓰기 위해서는 위에 name을 선언해줘야한다. 
+    }
 }
-const p1 = new Person; // object가 p1에 할당  
+const p1 = new Person("Mark"); // object가 p1에 할당  
 console.log(p1);
