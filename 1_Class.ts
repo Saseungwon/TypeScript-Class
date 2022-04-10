65f38729f7e447577ceac5b86c6e515eaa71ea8f
dfg1425@naver.com <dfg1425@naver.com>
2022-04-10 11:16:16 +0900

    Class

--- 1_Class.ts
+++ 1_Class.ts
@@ -1,6 +1,7 @@
 class Person {
   name;
 
+  
   constructor(name: string){    // 생성자 - constructor -> 이걸 해야 ("Mark") 같이 넣을 수 있다. 
     this.name = name;   // name을 프로퍼티로 쓰기 위해서는 위에 name을 선언해줘야한다. 
   }
